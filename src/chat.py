6ff79f254f2c2fb95bb3f508a1be97ce5931a44a
Wildrimak de Souza Pereira <finalwildrimak@gmail.com>
2026-09-12 01:26:03 -0300

    Parte relacionada a chat com RAG finalizada

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,21 @@
+import os
 from search import search_prompt
 
-def main():
-    chain = search_prompt()
+# Garantir que as variáveis de ambiente estão setadas
+for k in ("OPENAI_API_KEY", "DATABASE_URL","PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
 
-    if not chain:
-        print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
-        return
-    
-    pass
+def main():
+    while True:
+        
+        pergunta = input("Faça sua pergunta: ")
+        
+        if pergunta.lower() == "sair":
+            break
+
+        print("PERGUNTA: ", pergunta)
+        print(f"RESPOSTA: {search_prompt(pergunta)}")
 
 if __name__ == "__main__":
     main()
--- src/search.py
+++ src/search.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_postgres import PGVector
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -26,4 +35,30 @@ RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
 def search_prompt(question=None):
-    pass
+    
+    embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_EMBEDDING_MODEL","text-embedding-3-small"))
+    
+    # Criar store para os documentos
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    # Vetorizando a pergunta e fazendo a busca com 10 resultados
+    results = store.similarity_search_with_score(question, k=10)
+
+    # Concatenando os chunks em uma String
+    contexto = ""
+    for result in results:
+        chunk = result[0].page_content
+        contexto += chunk + "\n\n--\n\n"
+
+    query = PROMPT_TEMPLATE.format(contexto=contexto, pergunta=question)
+
+    chat = ChatOpenAI(model="gpt-5-nano", temperature=0.1)
+    resultado = chat.invoke(query)
+    resposta = resultado.content
+    
+    return resposta
